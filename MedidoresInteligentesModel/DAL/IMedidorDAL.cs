64014d885b933784af245d515b06695e3fd1be8f
Agustin Muñoz <112513840+Agustinimz@users.noreply.github.com>
2023-06-30 04:12:12 -0400

    part2

---
+++ MedidoresInteligentesModel/DAL/IMedidorDAL.cs
@@ -0,0 +1,16 @@
+﻿using MedidoresInteligentesModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedidoresInteligentesModel.DAL
+{
+    public interface IMedidorDAL
+    {
+        List<Medidor> ObtenerMedidores();
+
+        void Agregar(Medidor medidores);
+    }
+}
